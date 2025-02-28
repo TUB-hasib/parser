457d7f542ad54dfddf2b8e8c34e2a601cdd7038c
TUB-Hasib <h.alam@tu-berlin.de>
2025-02-28 08:47:19 +0100

    optional arg with required

--- read.py
+++ read.py
@@ -10,9 +10,10 @@ def readRedis(url="localhost", port=6379):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Set a Redis key with expiration.")
-    parser.add_argument("host", type=str, help="host name")
-    parser.add_argument("port", type=int, help="port number")
+    parser.add_argument("-H", "--host", type=str, required=True, help="host name")
+    parser.add_argument("-P", "--port", type=int, required=True, help="port number")
     args = parser.parse_args()
 
     readRedis(args.host, args.port)
+
 
